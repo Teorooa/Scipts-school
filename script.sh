8bc0e09c4884937afa5f038b162a50fa440e6d29
Teorooa <101207025+Teorooa@users.noreply.github.com>
2022-03-23 23:16:26 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 
+numero=num
+numero1=num1
 
 menu() {
 
-printf "\e[1;93m [\e1\e[1;93m]\e If\e[1;93m..[0m\n"
-printf "\e[1;93m [\e1\e[1;93m]\e While\e[1;93m..[0m\n"
-printf "\e[1;93m [\e1\e[1;93m]\e For\e[1;93m..[0m\n"
-printf "\e[1;93m [\e1\e[1;93m]\e Do\e[1;93m..[0m\n"
+read -p $" Select a option" option
 
-read -p $"Select a option" var
+if [[ $option == 1 ]]; then
+option2
+exit 1
+else
+read -p "Select a option"
+sleep 1
+exit 1
+menu
+fi
+}
+
+option2() {
+
+echo -e " = While"
+echo -e " "
+echo -e "#!bin/bash"
+echo -e " "
+echo -e "isvalid=true"
+echo -e "count=1"
+echo -e "while [ $isvalid ]"
+echo -e "do"
+echo -e "echo $count"
+echo -e "if [ $num -eq "" ];"
+echo -e "then"
+echo -e "breack"
+echo -e "fi"
+echo -e "((count++))"
+echo -e "done"
+echo -e "Agraga un numero del 1 al 9 en las comillas:" option1
+read -p $"Select a option" option1
+if [[ $option1 == 3 ]]; then
+menu
+else
+exit
+option2
+fi
+}
+menu
